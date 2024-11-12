46aa74402480f408c59d2a21a2dd981e0c26121b
hieucaotrantrong <hieu98861@donga.edu.vn>
2024-11-12 14:18:19 +0700

    spring framework

--- demo-database/demo-database/src/main/java/com/example/demo_database/controller/UserController.java
+++ demo-database/demo-database/src/main/java/com/example/demo_database/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo_database.Request.ApiResponse;
 import com.example.demo_database.Request.UserCreationRequest;
 import com.example.demo_database.Request.UserUpdateRequest;
 import com.example.demo_database.entity.User;
@@ -27,9 +28,10 @@ public class UserController {
     private UserService userService;
 
     @PostMapping
-    User createUser(@RequestBody @Valid UserCreationRequest request) {
-        return userService.createUser(request);
-
+    ApiResponse<User> createUser(@RequestBody @Valid UserCreationRequest request) {
+        ApiResponse<User> apiResponse = new ApiResponse<>();
+        apiResponse.setResult(userService.createUser(request));
+        return apiResponse;
     }
 
     @GetMapping
--- demo-database/demo-database/src/main/java/com/example/demo_database/exception/GlobalExceptionHandler.java
+++ demo-database/demo-database/src/main/java/com/example/demo_database/exception/GlobalExceptionHandler.java
@@ -5,11 +5,17 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.example.demo_database.Request.ApiResponse;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(value = RuntimeException.class)
-    ResponseEntity<String> handRuntimeException(RuntimeException exception) {
-        return ResponseEntity.badRequest().body(exception.getMessage());
+    ResponseEntity<ApiResponse> handRuntimeException(RuntimeException exception) {
+        ApiResponse apiResponse = new ApiResponse();
+        apiResponse.setCode(1001);
+        apiResponse.setMessage(exception.getMessage());
+
+        return ResponseEntity.badRequest().body(apiResponse);
     }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
